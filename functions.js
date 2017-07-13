9fbe11752055e772fdc80d8d0722f394bc61f087
HarrisonJASC4 <harjo2001@gmail.com>
2017-07-12 22:24:33 -0700

    Function pratice and rand() pratice

---
+++ functions.js
@@ -0,0 +1,10 @@
+function welloHorld(){
+    console.log("wello, Horld");
+}
+welloHorld();
+
+function hello(name){
+    console.log("hey, " + name + ".");
+}
+
+hello("bud");
--- harrison.js
+++ harrison.js
@@ -1,2 +1,22 @@
-console.log("Warriors blew a 3-1 Lead");
-console.log("Cavs in 7");
+//Task
+//Create a rand() function
+//Takes a number
+//Returns a random number between 0 and that number
+
+//PSEUDOCODE
+//multiply Math.random() by the given nmber
+//remove the decimal part
+//return the remaining intege
+
+function rand(num) {
+    //returns a random integer between 0 and num
+    var randomNum = num * Math.random();
+    var result = Math.floor(randomNum);
+    return result;
+}
+//Time consuming & save code way:
+    //return Math.floor(num * Math.random());
+
+console.log(rand(15));
+console.log(rand(999));
+console.log(rand(0));
